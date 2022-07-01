95121145386f15b0a0dfbb9e5b1c51d9674da456
dalenewman <dalenewman@gmail.com>
2022-06-30 20:25:54 -0400

    general maintenance

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -2,6 +2,6 @@ nuget pack Transformalize.Provider.GeoJson.nuspec -OutputDirectory "c:\temp\modu
 nuget pack Transformalize.Provider.GeoJson.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.GeoJson.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.0.8.30-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.Autofac.0.8.30-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.Autofac.v3.0.8.30-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.0.10.0-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.Autofac.0.10.0-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.Autofac.v3.0.10.0-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -2,6 +2,6 @@ nuget pack Transformalize.Provider.GeoJson.nuspec -OutputDirectory "c:\temp\modu
 nuget pack Transformalize.Provider.GeoJson.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.GeoJson.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.0.8.15-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.Autofac.0.8.15-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.Autofac.v3.0.8.15-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.0.10.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.Autofac.0.10.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.Autofac.v3.0.10.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
